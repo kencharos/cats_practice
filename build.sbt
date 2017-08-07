3b386bd6c3f11034b0ef86c9909b997e12cd1f56
kentaro.maeda <many.front@gmail.com>
2017-08-07 15:18:52 +0900

    first commit, type classes and instance basic

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "catz_start"
+
+version := "1.0"
+
+scalaVersion := "2.12.2"
+
+libraryDependencies := Seq(
+    "org.typelevel" %% "cats-core" % "1.0.0-MF",
+    "org.typelevel" %% "cats-free" % "1.0.0-MF",
+    "org.typelevel" %% "cats-testkit" % "1.0.0-MF",
+    "org.typelevel" %% "cats-effect" % "0.4")
---
+++ src/main/scala/FirstCatz.scala
@@ -0,0 +1,49 @@
+/**
+  * Created by kentaro.maeda on 2017/08/07.
+  *
+  * Catz を始めるにあたり、型クラス in Scala の基本。
+  */
+object FirstCatz extends App{
+  // 普通の fold の定義。
+  def sumInts(list: List[Int]): Int = list.foldRight(0)(_ + _)
+
+  def concatStrings(list: List[String]): String = list.foldRight("")(_ ++ _)
+
+  def unionSets[A](list: List[Set[A]]): Set[A] = list.foldRight(Set.empty[A])(_ union _)
+
+  // trait モノイドの導入
+  trait Monoid[A] {
+    def empty: A
+    def combine(x: A, y: A): A
+  }
+
+  // モノイドの int 実装
+  implicit val intAdditionMonoid: Monoid[Int] = new Monoid[Int] {
+    def empty: Int = 0
+    def combine(x: Int, y: Int): Int = x + y
+  }
+  // String 実装
+  implicit val StringAdditionMonoid: Monoid[String] = new Monoid[String] {
+    def empty: String = ""
+    def combine(x: String, y: String): String = x + y
+  }
+
+  // fold の汎用化。Monoid 実装について動くようになる。
+  def combineAll[A](list: List[A])(implicit A: Monoid[A]): A = list.foldRight(A.empty)(A.combine)
+
+  println(combineAll(List(1,2,3,4,4)))
+  println(combineAll(List("2", "3")))
+
+  // ただの pair
+  final case class Pair[A, B](first: A, second: B)
+  // Monoid pairの生成メソッド。defの引数も implicit にするのがポイント。
+  implicit def deriveMonoidPair[A, B](implicit A: Monoid[A], B: Monoid[B]): Monoid[Pair[A, B]] =
+    new Monoid[Pair[A, B]] {
+      def empty: Pair[A, B] = Pair(A.empty, B.empty)
+
+      def combine(x: Pair[A, B], y: Pair[A, B]): Pair[A, B] =
+        Pair(A.combine(x.first, y.first), B.combine(x.second, y.second))
+    }
+
+  println(combineAll(List(Pair(1,"A"), Pair(3, "B"))))
+}
