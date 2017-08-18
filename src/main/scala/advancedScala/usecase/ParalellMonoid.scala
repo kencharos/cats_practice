a621eb2416e5e32752eb9c99dedec4f67bc9605f
kentaro.maeda <many.front@gmail.com>
2017-08-18 20:27:40 +0900

    learn paralell monoid and free

---
+++ src/main/scala/advancedScala/usecase/ParalellMonoid.scala
@@ -0,0 +1,52 @@
+package advancedScala.usecase
+
+import java.util.concurrent.Executors
+
+import scala.concurrent.Await
+
+
+/**
+  * Created by kentaro.maeda on 2017/08/18.
+  */
+object ParalellMonoid extends App{
+
+  import cats.implicits._
+  import cats.Monoid
+  // ex
+  //a sequence of type Vector[A];
+  // a function of type A => B, where there is a Monoid for B;
+  def foldMap[A,B : Monoid](vector: Vector[A])(f: A => B):B = {
+    vector.foldLeft(Monoid[B].empty)((b, a) => Monoid[B].combine(b, f(a)))
+  }
+
+  println(foldMap(Vector(1,2,3,4))(_.toString))
+  println(foldMap("Hello world!".toVector)(_.toString.toUpperCase))
+
+  import scala.concurrent.Future
+  import scala.concurrent.duration._
+  //implicit val ex =  scala.concurrent.ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(8))
+  import scala.concurrent.ExecutionContext.Implicits.global
+  //
+  def parallelFoldMap[A, B : Monoid](values: Vector[A])(func: A => B): Future[B] = {
+    val cpu = Runtime.getRuntime.availableProcessors
+    val splited = values.grouped(values.size/cpu)
+    val parB = splited.map{v =>  Future(foldMap(v)(func)).map(a => {println(s"start${Thread.currentThread()}");a;})}
+    Future.sequence(parB).map(_.foldLeft(Monoid[B].empty)(_ |+| _) )
+  }
+
+  // in cats
+  def parallelFoldMap2[A, B: Monoid] (values: Vector[A])(func: A => B): Future[B] = {
+    val numCores = Runtime.getRuntime.availableProcessors
+    val groupSize = (1.0 * values.size / numCores).ceil.toInt
+    values
+      .grouped(groupSize)
+      .toVector // Vector[Iterator[A]]
+      .traverse(group => Future(group.toVector.foldMap(func))) // Future[Vector[B]]
+      .map(_.combineAll)  // Futuer[B]
+  }
+
+  println(Await.result(parallelFoldMap((1 to 1000000).toVector)(identity), 3.seconds))
+  println(Await.result(parallelFoldMap2((1 to 1000000).toVector)(identity), 3.seconds))
+
+
+}
---
+++ src/main/scala/advancedScala/usecase/PrepareFree.scala
@@ -0,0 +1,106 @@
+package advancedScala.usecase
+
+import cats.free.Free
+
+
+/**
+  * Created by kentaro.maeda on 2017/08/18.
+  */
+object PrepareFree extends App{
+
+  // ADTを用意
+  sealed trait KVStoreA[A]
+  case class Put[T](key: String, value: T) extends KVStoreA[Unit]
+  case class Get[T](key: String) extends KVStoreA[Option[T]]
+  case class Delete(key: String) extends KVStoreA[Unit]
+
+  import cats.free.Free
+  import cats.free.Free.liftF
+
+  // Stateのように、ADTを Freeでくるむ。
+  type KVStore[A] = Free[KVStoreA, A]
+
+  // Put returns nothing (i.e. Unit).
+  // liftFは、ADTを Freeに上げる。
+  // 各関数の結果を、ADTの各型にマッピングする。
+  def put[T](key: String, value: T): KVStore[Unit] =
+    liftF[KVStoreA, Unit](Put[T](key, value))
+
+  // Get returns a T value.
+  def get[T](key: String): KVStore[Option[T]] =
+    liftF[KVStoreA, Option[T]](Get[T](key))
+
+  // Delete returns nothing (i.e. Unit).
+  def delete(key: String): KVStore[Unit] =
+    liftF(Delete(key))
+
+  // Update composes get and set, and returns nothing.
+  def update[T](key: String, f: T => T): KVStore[Unit] =
+    for {
+      vMaybe <- get[T](key)
+      _ <- vMaybe.map(v => put[T](key, f(v))).getOrElse(Free.pure(()))
+    } yield ()
+
+  // 実際のプログラム。メソッド呼び出しが、Freeインスタンスのネストに変わる。
+  def program: KVStore[Option[Int]] =
+    for {
+      _ <- put("wild-cats", 2)
+      _ <- update[Int]("wild-cats", (_ + 12))
+      _ <- put("tame-cats", 5)
+      n <- get[Int]("wild-cats")
+      _ <- delete("tame-cats")
+    } yield n
+
+  println(program) // Free(...)
+
+  // programを解釈して実行するインタプリタを定義する。
+  // インタプリタの内容次第で色々結果を作れる。
+  import cats.arrow.FunctionK
+  import cats.{Id, ~>}
+  // ~> は FunctionK, FuntcionK は、 M[_] => M'[_] のように、モナドに対する変換を定義する。
+  // 今回は単なる値A が欲しいので、Idだが、Futurer とか色々できる。
+  import scala.collection.mutable
+
+  // the program will crash if a key is not found,
+  // or if a type is incorrectly specified.
+  def impureCompiler: KVStoreA ~> Id  = new (KVStoreA ~> Id) {
+
+    // a very simple (and imprecise) key-value store
+    val kvs = mutable.Map.empty[String, Any]
+    // ADTの型に応じたプログラムを書いていける。
+    def apply[A](fa: KVStoreA[A]): Id[A] = //applyの戻り値が、 ~>の右辺のIdになる。
+      fa match {
+        case Put(key, value) =>
+          println(s"put($key, $value)")
+          kvs(key) = value
+          ()
+        case Get(key) =>
+          println(s"get($key)")
+          kvs.get(key).map(_.asInstanceOf[A])
+        case Delete(key) =>
+          println(s"delete($key)")
+          kvs.remove(key)
+          ()
+      }
+  }
+
+  // インタプリターを programに対してfoldMapで指定すると実行。
+  val result: Option[Int] = program.foldMap(impureCompiler)
+  println(result)
+
+  // 複数のインタプリターを切り替えできる。
+  import cats.data.State
+
+  type KVStoreState[A] = State[Map[String, Any], A]
+  val pureCompiler: KVStoreA ~> KVStoreState = new (KVStoreA ~> KVStoreState) {
+    def apply[A](fa: KVStoreA[A]): KVStoreState[A] =
+      fa match {
+        case Put(key, value) => State.modify(_.updated(key, value))
+        case Get(key) =>
+          State.inspect(_.get(key).map(_.asInstanceOf[A]))
+        case Delete(key) => State.modify(_ - key)
+      }
+  }
+  val result2: (Map[String, Any], Option[Int]) = program.foldMap(pureCompiler).run(Map.empty).value
+  println(result2)
+}
