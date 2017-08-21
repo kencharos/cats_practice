e4e4308498e8c6920565b5e70738f86b6fa973fb
kentaro.maeda <many.front@gmail.com>
2017-08-21 10:22:16 +0900

    learn end

--- build.sbt
+++ build.sbt
@@ -11,3 +11,10 @@ libraryDependencies := Seq(
     "org.typelevel" %% "cats-free" % "1.0.0-MF",
     "org.typelevel" %% "cats-testkit" % "1.0.0-MF",
     "org.typelevel" %% "cats-effect" % "0.4")
+
+// Kind 解決のためのプラグイン
+addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.4")
+// マクロでボイラープレート生成のためのプラグイン
+addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
+
+libraryDependencies += "com.github.mpilquist" %% "simulacrum" % "0.10.0"
--- src/main/scala/advancedScala/usecase/CRDT2.scala
+++ src/main/scala/advancedScala/usecase/CRDT2.scala
@@ -1,16 +1,20 @@
 package advancedScala.usecase
 
-import cats.kernel.Monoid
+import advancedScala.usecase.GCounterExample.{GCounter, KeyValueStore}
 
-object CRDT2 extends App{
 
-  // A,B,Cごとにカウントし、最大値に寄せるもの。
+object GCounterExample {
+  import cats.{Monoid, Foldable}
+  import cats.syntax.foldable._
+  import cats.syntax.semigroup._
+  import scala.language.higherKinds
 
-  // モノイド拡張。結合ではなく、どちらか大きい方を取るもの。
+
   trait BoundedSemiLattice[A] extends Monoid[A] {
     def combine(a1: A, a2: A): A
     def empty: A
   }
+  // Maxを取る、Monoid拡張
   object BoundedSemiLattice {
     implicit object intBoundedSemiLatticeInstance extends
       BoundedSemiLattice[Int] {
@@ -23,75 +27,74 @@ object CRDT2 extends App{
       new BoundedSemiLattice[Set[A]]{
         def combine(a1: Set[A], a2: Set[A]): Set[A] =
           a1 union a2
-        val empty: Set[A] = Set.empty[A]
+        val empty: Set[A] =
+          Set.empty[A]
       }
   }
-
-  import cats.instances.list._
-  import cats.instances.map._
-  import cats.syntax.foldable._
-  import cats.syntax.semigroup._
-
-  import scala.language.higherKinds
-
-  //GCounterをさらに抽象化する。MAPを切り替え可能に
-
-  // Trait化。
+  //F[_,_] で K,V 2つをとるKind
   trait GCounter[F[_,_],K, V] {
-    def increment(f: F[K, V])(k: K, v: V)(implicit m: Monoid[V]):
-    F[K, V]
+    def increment(f: F[K, V])(key: K, value: V)(implicit m: Monoid[V]): F[K, V]
     def total(f: F[K, V])(implicit m: Monoid[V]): V
-    def merge(f1: F[K, V], f2: F[K, V])(implicit b: BoundedSemiLattice[V]): F[K, V]
+    def merge(f1: F[K, V], f2: F[K, V])(implicit b:
+    BoundedSemiLattice[V]): F[K, V]
   }
+  object GCounter {
+    def apply[F[_,_],K, V](implicit g: GCounter[F, K, V]) = g
 
-  // Mapに限らず、Mapと似たような性質を持つ CRDTのための型クラスをKVSとして定義する。
-  trait KeyValueStore[F[_,_]] {
-    def +[K, V](f: F[K, V])(key: K, value: V): F[K, V]
-    def get[K, V](f: F[K, V])(key: K): Option[V]
-    def getOrElse[K, V](f: F[K, V])(key: K, default: V): V =
-      get(f)(key).getOrElse(default)
-  }
-
-  object KeyValueStore {
-    // syntax
-    implicit class KeyValueStoreOps[F[_,_],K, V](f: F[K, V]) {
-      def +(key: K, value: V)(implicit kv: KeyValueStore[F]): F[K, V] =
-        kv.+(f)(key, value)
-      def get(key: K)(implicit kv: KeyValueStore[F]): Option[V] =
-        kv.get(f)(key)
-      def getOrElse(key: K, default: V)(implicit kv: KeyValueStore
-        [F]): V =
-        kv.getOrElse(f)(key, default)
+    implicit class GCounterOps[F[_,_],K, V](f: F[K, V]) {
+      def increment(key: K, value: V)(implicit g: GCounter[F, K, V], m: Monoid[V]): F[K, V] =
+        g.increment(f)(key, value)
+      def total(implicit g: GCounter[F, K, V], m: Monoid[V]): V = g.total(f)
+      def merge(that: F[K, V])(implicit g: GCounter[F, K, V], b: BoundedSemiLattice[V]): F[K, V] =
+        g.merge(f, that)
     }
-    // map版の実装
+
+    implicit def keyValueInstance[F[_,_],K, V](implicit k: KeyValueStore[F],
+        // Foldable[F[K,A]]としたいが、型引数が合わないので、その場でl[A]型エイリアスを作って、F[l]としている。
+        // https://github.com/non/kind-projector の機能によるもの というわけではないのか。kind projectorを使うと、もっと短く書けるというだけ。
+        km: Monoid[F[K, V]], kf: Foldable[({type l[V]=F[K, V]})#l]
+        ): GCounter[F, K, V] =
+      new GCounter[F, K, V] {
+        import KeyValueStore._ // For KeyValueStore syntax
+        def increment(f: F[K, V])(key: K, value: V)(implicit m: Monoid[V]): F[K, V] = f + (key, (f.getOrElse(key, m.empty) |+| value))
+        def total(f: F[K, V])(implicit m: Monoid[V]): V = kf.foldMap(f)(identity)
+        def merge(f1: F[K, V], f2: F[K, V])(implicit b: BoundedSemiLattice[V]): F[K, V] = f1 |+| f2
+      }
+  }
+    // CRDTのため性質を定義したの型クラス。
+    trait KeyValueStore[F[_,_]] {
+      def +[K, V](f: F[K, V])(key: K, value: V): F[K, V]
+      def get[K, V](f: F[K, V])(key: K): Option[V]
+      def getOrElse[K, V](f: F[K, V])(key: K, default: V): V =
+        get(f)(key).getOrElse(default)
+    }
+    object KeyValueStore {
+      implicit class KeyValueStoreOps[F[_,_],K, V](f: F[K, V]) {
+        def +(key: K, value: V)(implicit kv: KeyValueStore[F]): F[K, V] =
+          kv.+(f)(key, value)
+        def get(key: K)(implicit kv: KeyValueStore[F]): Option[V] = kv.get(f)(key)
+        def getOrElse(key: K, default: V)(implicit kv:
+        KeyValueStore[F]): V =
+          kv.getOrElse(f)(key, default)
+      }
     implicit object mapKeyValueStoreInstance extends KeyValueStore[Map] {
       def +[K, V](f: Map[K, V])(key: K, value: V): Map[K, V] =
         f + (key, value)
       def get[K, V](f: Map[K, V])(key: K): Option[V] =
         f.get(key)
-      override def getOrElse[K, V](f: Map[K, V])(key: K, default:
-      V): V =
+      override def getOrElse[K, V](f: Map[K, V])(key: K, default: V): V =
         f.getOrElse(key, default)
     }
   }
+}
 
-  // 実装作成。
-  import cats.Foldable
-  import cats.syntax.semigroup._
-  import cats.syntax.foldable._
-/*
-  implicit def keyValueInstance[F[_,_],K, V](
-    implicit k: KeyValueStore[F],
-          km: Monoid[F[K, V]],
-          kf: Foldable[({type l[A]=F[K, A]})#l]): GCounter[F, K, V] =
-    new GCounter[F, K, V] {
-      import KeyValueStore._ // For KeyValueStore syntax
-      def increment(f: F[K, V])(key: K, value: V)(implicit m: Monoid[V]): F[K, V] =
-        f + (key, (f.getOrElse(key, m.empty) |+| value))
-      def total(f: F[K, V])(implicit m: Monoid[V]): V = f.foldMap(identity _)
-      def merge(f1: F[K, V], f2: F[K, V])(implicit b: BoundedSemiLattice[V]): F[K, V] = f1 |+| f2
-    }
-    */
-
-
+object CRDT2 extends App{
+  import cats.instances.map._
+  import cats.instances.int._
+  import KeyValueStore._
+  import GCounter._
+  // Mapに、KVSを接続している。
+  val crdt1 = Map("a" -> 1, "b" -> 3, "c" -> 5)
+  val crdt2 = Map("a" -> 2, "b" -> 4, "c" -> 6)
+  println(crdt1.increment("a", 20).merge(crdt2).total)
 }
